be9608c31086db46de78f5e1bb7f4a28b4d38ef3
webbdev <forrwebdev@gmail.com>
2016-03-05 20:40:52 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function() {
+
+    // Change "fa-plus/minus" icon on collapse
+    $('#footer_links-small .panel-collapse').on('shown.bs.collapse', function () {
+      $(this).prev().find(".fa").removeClass("fa-plus").addClass("fa-minus");
+    });
+    
+    $('#footer_links-small .panel-collapse').on('hidden.bs.collapse', function () {
+      $(this).prev().find(".fa").removeClass("fa-minus").addClass("fa-plus");
+    });
+
+});
